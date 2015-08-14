0972ea69339a101c2f767aac5e12b5cef25502f9
Seth Fitzsimmons <seth@mojodna.net>
2015-08-13 18:02:57 -0700

    Preview generation using abaculus + tilelive-cartodb

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import url from "url";
 
 import cartodbExport from "./lib/cartodb-export";
 import CartodbPreviewView from "./lib/cartodb-preview-view";
+import generatePreview from "./lib/generate-preview";
 import linter from "./lib/linter";
 import tileMillExport from "./lib/tilemill-export";
 
@@ -61,6 +62,7 @@ export default {
   activate: state => {
     atom.commands.add("atom-workspace", {
       "cartodb:preview": show,
+      "cartodb:render": generatePreview,
       "cartodb:cartodb-export": cartodbExport,
       "cartodb:tilemill-export": tileMillExport
     });
